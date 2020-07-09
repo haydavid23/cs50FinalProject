f280344b89f9808922226a515b817c945f5fb58f
David Hay <hay.david@hotmail.com>
2020-07-09 15:16:24 +0000

    tables revised

--- src/main.py
+++ src/main.py
@@ -2,14 +2,14 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify, url_for, send_file,json
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db
-from models import User, Teacher, Students, StudentsGrades, SubmitedAssigments
+from models import User, Teacher, Subjects, Students,SchoolTerm, StudentsGrades, SubmitedAssignments, AssignedAssignments
 
 #from models import Person
 
@@ -22,6 +22,9 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+UPLOAD_FOLDER = './src/pdf'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -33,38 +36,152 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@app.route('/getGradeBookGradeBysubject', methods=['POST', 'GET'])
+def handle_getGradeBookGradeBysubject():
     studentsArray = Students.query.all()
-    assigments = SubmitedAssigments.query.all()
+    assignments = SubmitedAssignments.query.all()
 
-    studentAssigments = []
+    studentAssignments = []
     student = {}
 
     for students in studentsArray:
         student['Student_Name']=students.name
-        for work in assigments:
+        for work in assignments:
              if work.studentId == students.id:
-                 student[work.assigmentName] = work.grade
-        studentAssigments.append(student)
-        print(studentAssigments)
+                 student[work.assignmentName] = work.grade
+        studentAssignments.append(student)
+        print(studentAssignments)
         student = {}
-    print(studentAssigments)
+    print(studentAssignments)
 
     response_body = {"hello":"hello"}
 
-    return jsonify(studentAssigments), 200
+    return jsonify(studentAssignments), 200
 
 
-@app.route('/add', methods=['POST', 'GET'])
-def handle_add():
+@app.route('/getAssignmentsBySubject', methods=['POST', 'GET'])
+def handle_assignmentsBySubject():
 
-    test = StudentsGrades(studentId=1, subject="math", gradeLevel="9", semester="q2", grade="A")
-    db.session.add(test)
+    subjectRequested = request.get_json()
+    subject = Subjects.query.filter_by(subject="Math").first()
+
+    assignments = AssignedAssignments.query.filter_by(subjectId=subject.id).all()
+
+    assignmentNames = []
+    
+    for assignment in assignments:
+        assignmentNames.append(assignment.name)
+
+    print(assignmentNames)
+
+    return jsonify(assignmentNames), 200
+
+def handle_getAllAssignmentsList():
+
+    arr = []
+
+    assignments = AssignedAssignments.query.all()
+    for assignment in assignments:
+        arr.append(assignment.serialize())
+
+    return jsonify(arr), 200
+
+
+
+@app.route('/edit', methods=['POST', 'GET'])
+def handle_edit():
+    assignments = AssignedAssignments.query.get(5)
+    assignments.name = "mathtest3"
     db.session.commit()
 
+    return "succcess", 200
 
-    return 'test', 200
+@app.route('/saveAssignedFile', methods=['POST', 'GET'])
+def handle_saveFile():
+
+    assignment =  request.files
+    form = json.loads(request.form['form'])
+    print(form['subject'])
+    print(assignment)
+
+    f = assignment['file']
+    path = os.path.join(app.config['UPLOAD_FOLDER'], f.filename)
+    f.save(path)
+    print(path)
+   
+
+    insertAssignment = AssignedAssignments(name=form["assignmentName"], 
+    subject=form['subject'], note=form['note'], assignmentFile=path)
+
+    db.session.add(insertAssignment)
+    db.session.commit()
+
+    return jsonify("SUCCESS"), 200
+
+
+@app.route('/getAllSubjects', methods=['GET'])
+def handle_getAllSubjects():
+
+    subjects = Subjects.query.all()
+
+    jsonSubjects = []
+
+    for subject in subjects:
+        jsonSubjects.append(subject.serialize())
+
+    return jsonify(jsonSubjects)
+
+
+@app.route('/getAssignmentPdf', methods=['POST', 'GET'])
+def handle_returnAssignmentPdf():
+
+    filePath = request.get_json()
+    print(filePath)
+
+    return send_file('pdf/samplePdf.pdf')
+
+
+@app.route('/setSchoolTerm', methods=['POST', 'GET'])
+def handle_setSchoolTerm():
+
+    currentTerm = SchoolTerm.query.filter_by(current = True).first()
+    schoolTerm = request.get_json()
+
+    if schoolTerm["schoolYear"] == "" or schoolTerm["quarter"] == "":
+        return jsonify("Please provide valid values")
+    else:
+        if currentTerm == None:
+
+            setTerm = SchoolTerm(schoolYear=schoolTerm["schoolYear"],quarter=schoolTerm['quarter'], current=True)
+            db.session.add(setTerm)
+            db.session.commit()
+ 
+            return jsonify("Success"), 200
+        else:
+            currentTerm.current = False
+            db.session.commit()
+
+            setTerm = SchoolTerm(schoolYear=schoolTerm["schoolYear"],quarter=schoolTerm['quarter'], current=True)
+            db.session.add(setTerm)
+            db.session.commit()
+
+            return jsonify("Success"), 200
+ 
+            
+
+
+@app.route('/getCurrentSchoolTerm', methods=['POST', 'GET'])
+def handle_getCurrentSchoolTerm():
+
+        currentTerm = SchoolTerm.query.filter_by(current = True).first()
+
+        if currentTerm == None:
+            return jsonify("No active school year")
+        else:
+            term = currentTerm.serialize()
+            return jsonify(term), 200
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -19,10 +20,9 @@ class Teacher(db.Model):
         return {
             "id": self.id,
             "name": self.name,
-            "name": self.lastName,
-            "name": self.gradeLevel,
-            "name": self.username,
-            "name": self.teacherId
+            "lastName": self.lastName,
+            "gradeLevel": self.gradeLevel,
+            "username": self.username
             # do not serialize the password, its a security breach
         }
 
@@ -41,9 +41,9 @@ class Students(db.Model):
         return {
             "id": self.id,
             "name": self.name,
-            "name": self.lastName,
-            "name": self.gradeLevel,
-            "name": self.username
+            "lastName": self.lastName,
+            "gradeLevel": self.gradeLevel,
+            "username": self.username
  
             # do not serialize the password, its a security breach
         }
@@ -52,14 +52,14 @@ class StudentsGrades(db.Model):
   
     id = db.Column(db.Integer, primary_key=True)
     studentId = db.Column(db.Integer,  nullable=False)
-    subject = db.Column(db.String(120), nullable=False)
+    subjectId = db.Column(db.Integer, nullable=False)
     gradeLevel = db.Column(db.String(80), unique=False, nullable=False)
     semesterId = db.Column(db.Integer, nullable=False)
     grade = db.Column(db.String(80), unique=False, nullable=False)
 
-    # __table_args__ = (
-    #     db.UniqueConstraint('studentId', 'subject','semesterId'),
-    # )
+    __table_args__ = (
+        db.UniqueConstraint('studentId', 'subjectId','semesterId'),
+    )
 
     def __repr__(self):
         return '<StudentsGrades %r>' % self.grade
@@ -67,50 +67,62 @@ class StudentsGrades(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "name": self.studentId,
-            "name": self.subject,
-            "name": self.gradeLevel,
-            "name": self.grade
+            "studentId": self.studentId,
+            "subjectId": self.subjectId,
+            "gradeLevel": self.gradeLevel,
+            "grade": self.grade  
  
             # do not serialize the password, its a security breach
         }
 
 
 
-class SubmitedAssigments(db.Model):
+class SubmitedAssignments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     studentId = db.Column(db.Integer, unique=False, nullable=False)
-    subject = db.Column(db.String(100), unique=False, nullable=False)
-    assigmentName = db.Column(db.String(100), unique=False, nullable=False)
+    subjectId = db.Column(db.Integer, unique=False, nullable=False)
+    assignmentName = db.Column(db.String(100), unique=False, nullable=False)
     grade = db.Column(db.Float, unique=False, nullable=True)
+    assignmentFile = db.Column(db.String(80), unique=False, nullable=True)
+    submitedDate = db.Column(db.DateTime,unique=True, nullable=True)
     semesterId = db.Column(db.Integer, unique=False, nullable=True)
 
+    __table_args__ = (
+        db.UniqueConstraint('studentId', 'subjectId','assignmentName'),
+    )
+
+
     def __repr__(self):
-        return '<SubmitedAssigments %r>' % self.assigmentName
+        return '<SubmitedAssignments %r>' % self.assignmentName
 
     def serialize(self):
         return {
             "id": self.id,
-            "subject": self.subject,
+            "subjectId": self.subjectId,
             "studentId": self.studentId,
-            "assigmentName": self.assigmentName,
+            "assignmentName": self.assignmentName,
+            "submitedDate": self.submitedDate,
+            "assignmentFile": self.assignmentFile,
             "grade": self.grade
 
             # do not serialize the password, its a security breach
         }
 
-class Semester(db.Model):
+class SchoolTerm(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     quarter= db.Column(db.String(20), unique=False, nullable=False)
     schoolYear = db.Column(db.String(20), unique=False, nullable=False)
-
+    current = db.Column(db.Boolean, unique=False, nullable=False)
+    
+    __table_args__ = (
+        db.UniqueConstraint('quarter', 'schoolYear'),
+    )
 
     def __repr__(self):
         return '<Semester %r>' % self.schoolYear
 
     def serialize(self):
         return {
-            "id": self.id,
             "quarter": self.quarter,
             "schoolYear": self.schoolYear,
 
@@ -119,25 +131,44 @@ class Semester(db.Model):
         }
 
 
-class AssignedAssigments(db.Model):
+class AssignedAssignments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True, nullable=True)
-    subject = db.Column(db.String(10), unique=False, nullable=True)
-    assigmentFile = db.Column(db.LargeBinary, unique=False, nullable=True)
+    subjectId = db.Column(db.Integer, unique=False, nullable=True)
+    note = db.Column(db.String(80), unique=False, nullable=True)
+    assignmentFile = db.Column(db.String(80), unique=False, nullable=True)
     dueDate = db.Column(db.DateTime, unique=False, nullable=True)
     semesterId = db.Column(db.Integer, unique=False, nullable=True)
+    submittable  = db.Column(db.Boolean, unique=False, nullable=False)
 
     def __repr__(self):
-        return '<AssignedAssigments %r>' % self.name
+        return '<AssignedAssignments %r>' % self.name
 
     def serialize(self):
         return {
-            "name": self.id,
-            "subject": self.subject,
+            "id": self.id,
+            "name":self.name,
+            "subjectId": self.subjectId,
             "dueDate": self.dueDate,
-            "semesterId": self.semesterId
+            "note": self.note,
+            "assignmentFile": self.assignmentFile,
+            "submittable": self.submittable,
+            "semesterId":self.semesterId
+            }
 
-           
+
+class Subjects(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(30), unique=True, nullable=False)
+
+
+    def __repr__(self):
+        return '<Subjects %r>' % self.subject
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "subject": self.subject
         }
 
 class User(db.Model):
